87c2584d7b6552fad8e202fbd529130469032f7e
CypherChris-007 <chrisb.college1010@gmail.com>
2026-06-26 20:06:03 -0500

    Ollama Ai agent bug fix | added CSV file data | fixed Ai agent response

---
+++ AI Agent/main.py
@@ -0,0 +1,21 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+    You are an expert in answering questions about a pizza restaurant: {reviews}
+    Here are some relevant reviews: {questions}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model 
+
+while True:
+    print("\n-----------------------------")
+    question = input("Ask a question (q to quit): ")
+    print("\n-----------------------------")
+    if question.lower() == "q":
+        break
+
+result = chain.invoke({"reviews": [],  "question": question})
+print(result)
---
+++ AI Agent/vector.py
@@ -0,0 +1,5 @@
+from langchain_ollama.llms import OllamaEmbeddings
+from langchain_chroma import chroma
+from langchain_core.documents import Documents
+import os
+import pandas as pd
